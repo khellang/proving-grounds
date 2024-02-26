2411824a0c5bb37e55c172a89942bef41a6a8407
Kristian Hellang <kristian@hellang.com>
2024-02-26 15:10:13 +0100

    Load scene

--- frontend/src/components/render-view.tsx
+++ frontend/src/components/render-view.tsx
@@ -3,6 +3,28 @@
 import { useEffect, useRef } from "react";
 import { getDeviceProfile } from "@novorender/api";
 import { View } from "@novorender/api";
+import { SceneData, createAPI } from "@novorender/data-js-api";
+
+const dataApi = createAPI({
+  serviceUrl: "https://data.novorender.com/api",
+});
+
+const loadPublicScene = async (view: View, id: string) => {
+  const sceneData = await dataApi.loadScene(id);
+
+  // Destructure relevant properties into variables
+  const { url: _url } = sceneData as SceneData;
+  const url = new URL(_url);
+  const parentSceneId = url.pathname.replaceAll("/", "");
+  url.pathname = "";
+
+  // load the scene using URL gotten from `sceneData`
+  const config = await view.loadScene(url, parentSceneId, "index.json");
+
+  const { center, radius } = config.boundingSphere;
+
+  view.activeController.autoFit(center, radius);
+};
 
 export default function RenderView() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -23,6 +45,8 @@ export default function RenderView() {
       const view = new View(canvasRef.current, deviceProfile, imports);
 
       view.modifyRenderState({ grid: { enabled: true } });
+
+      await loadPublicScene(view, "95a89d20dd084d9486e383e131242c4c");
 
       await view.run();
 
