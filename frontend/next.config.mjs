198a5bff9b2df014eb571ee237906c36667b6791
Kristian Hellang <kristian@hellang.com>
2024-02-26 13:16:31 +0100

    Copy Novorender resources to public directory

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,24 @@
+import CopyPlugin from "copy-webpack-plugin";
+import { join, dirname } from "path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: join(__dirname, "node_modules/@novorender/api/public/*"),
+            to: join(__dirname, "public/novorender/api/[name][ext]"),
+          },
+        ],
+      })
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
