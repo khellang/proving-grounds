0b23a0e500a0592e146deaf0fbf26c2aa2f9cc98
Kristian Hellang <kristian@hellang.com>
2024-02-28 23:16:48 +0100

    Add theme provider for dark mode support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
-import RenderView from "@/components/RenderView";
+import RenderView from "@/components/render-view";
+import Toolbar from "@/components/toolbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
-  return <RenderView sceneId="3b5e65560dc4422da5c7c3f827b6a77c" />;
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className="relative">
+        <Toolbar className="absolute inset-4" />
+        <RenderView sceneId="3b5e65560dc4422da5c7c3f827b6a77c" />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
